181964ceef5e9c6bd4ca2cdd3056e9eb5bd7f2b2
Booze33 <tebetisloh@gmail.com>
2023-03-06 18:49:31 -0600

    fixing bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector('.hamburger');
-const menuNav = document.querySelector(".mobile-nav");
+const menuNav = document.querySelector('.mobile-nav');
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('is-active');
   menuNav.classList.toggle('is-active');

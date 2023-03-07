84204466121301a2c6ffc5bb00ae2fa75e2698e3
Booze33 <tebetisloh@gmail.com>
2023-03-06 18:34:15 -0600

    fixiing bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menu_btn = document.querySelector('.hamburger');
-const menu_nav = document.querySelector('.mobile-nav');
-menu_btn.addEventListener('click', function (){
-    menu_btn.classList.toggle('is-active')
-    menu_nav.classList.toggle('is-active')
-})
+const menuBtn = document.querySelector('.hamburger');
+const menuNav = document.querySelector('.mobile-nav');
+menu_btn.addEventListener('click', () => {
+  menu_btn.classList.toggle('is-active');
+  menu_nav.classList.toggle('is-active')
+});

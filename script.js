9e1681bf2eb59fbfb7cb9e14d7fd5e28f435de65
Booze33 <tebetisloh@gmail.com>
2023-03-09 19:03:30 -0600

    fixing bugs

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ menuBtn.addEventListener('click', () => {
   menuNav.classList.toggle('is-active');
 });
 
-let projects = {
+const projects = {
   firstDiv: {
     name: 'Tonic',
     image: 'images/Snapshoot Portfolio (3).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -19,7 +19,7 @@ let projects = {
   secondDiv: {
     name: 'Tonic',
     image: 'images/Snapshoot Portfolio.png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -27,7 +27,7 @@ let projects = {
   thirdDiv: {
     name: 'Tonic',
     image: 'images/Snapshoot Portfolio (1).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -35,7 +35,7 @@ let projects = {
   fourthDiv: {
     name: 'Tonic',
     image: 'images/Snapshoot Portfolio (2).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -43,7 +43,7 @@ let projects = {
 };
 
 // first div
-  const click = document.querySelector('.click');
+const click = document.querySelector('.click');
 const popup = document.querySelector('.popup');
 popup.innerHTML = `
 <div class="head">
@@ -91,11 +91,9 @@ remove.addEventListener('click', () => {
   remove.classList.remove('active');
   popup.classList.remove('active');
   overlay2.classList.remove('active');
-})
+});
 
-
-
-//second div
+// second div
 const clickSecond = document.querySelector('.click-2');
 const popupSecond = document.querySelector('.popup-2');
 popupSecond.innerHTML = `
@@ -131,21 +129,20 @@ linksSecond.innerHTML = `
 </div>
 `;
 popupSecond.appendChild(linksSecond);
-const removeSecond = document.querySelector('.bar-2')
-
+const removeSecond = document.querySelector('.bar-2');
 
 clickSecond.addEventListener('click', () => {
   clickSecond.classList.add('active');
   popupSecond.classList.add('active');
   overlay2.classList.add('active');
-})
-  removeSecond.addEventListener('click', () => {
-    removeSecond.classList.remove('active')
-    popupSecond.classList.remove('active')
-    overlay2.classList.remove('active');
+});
+removeSecond.addEventListener('click', () => {
+  removeSecond.classList.remove('active');
+  popupSecond.classList.remove('active');
+  overlay2.classList.remove('active');
 });
 
-//third div
+// third div
 const clickThird = document.querySelector('.click-3');
 const popupThird = document.querySelector('.popup-3');
 popupThird.innerHTML = `
@@ -183,19 +180,18 @@ linksThird.innerHTML = `
 popupThird.appendChild(linksThird);
 const removeThird = document.querySelector('.bar-3');
 
-
 clickThird.addEventListener('click', () => {
   clickThird.classList.add('active');
   popupThird.classList.add('active');
   overlay2.classList.add('active');
 });
-  removeThird.addEventListener('click', () => {
-    removeThird.classList.remove('active');
-    popupThird.classList.remove('active');
-    overlay2.classList.remove('active');
+removeThird.addEventListener('click', () => {
+  removeThird.classList.remove('active');
+  popupThird.classList.remove('active');
+  overlay2.classList.remove('active');
 });
 
-//fourth div
+// fourth div
 const clickFourth = document.querySelector('.click-4');
 const popupFourth = document.querySelector('.popup-4');
 popupFourth.innerHTML = `
@@ -239,12 +235,11 @@ clickFourth.addEventListener('click', () => {
   popupFourth.classList.add('active');
   overlay2.classList.add('active');
 });
-  removeFourth.addEventListener('click', () => {
-    removeFourth.classList.remove('active');
-    popupFourth.classList.remove('active');
-    overlay2.classList.remove('active');
+removeFourth.addEventListener('click', () => {
+  removeFourth.classList.remove('active');
+  popupFourth.classList.remove('active');
+  overlay2.classList.remove('active');
 });
-
 
 // Desktop popUp
 
@@ -252,7 +247,7 @@ const desktop = {
   desktopFst: {
     name: 'Tonic',
     image: 'images/Snapshoot Portfolio (3).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -260,7 +255,7 @@ const desktop = {
   desktopSec: {
     name: 'Multi-Post Stores',
     image: 'images/Snapshoot Portfolio (3).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -268,7 +263,7 @@ const desktop = {
   desktopTrd: {
     name: 'Facebook 360',
     image: 'images/Snapshoot Portfolio (3).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
@@ -276,18 +271,18 @@ const desktop = {
   desktopFth: {
     name: 'Uber Navigation',
     image: 'images/Snapshoot Portfolio (3).png',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     language: ['html', 'css', 'javaScript'],
     live: 'https://booze33.github.io/My-Portfolio/',
     source: 'https://github.com/Booze33/My-Portfolio/tree/main',
-  }
+  },
 };
 
 // desktop first div
 
 const post = document.querySelector('.this');
 const task = document.createElement('div');
-  task.className = 'task1';
+task.className = 'task1';
 task.innerHTML = `
 <div class="cross">
   <h2 class="h2-tag">${desktop.desktopFst.name}</h2>
@@ -328,22 +323,23 @@ task.innerHTML = `
 `;
 body.append(task);
 
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
 post.addEventListener('click', () => {
   post.classList.add('active');
   task.classList.add('active');
   overlay.classList.add('active');
-})
+});
 const close = document.querySelector('.ex');
-close.addEventListener('click',() => {
+close.addEventListener('click', () => {
   close.classList.remove('active');
   task.classList.remove('active');
   overlay.classList.remove('active');
-})
-//desktop second div
+});
+
+// desktop second div
 const post2 = document.querySelector('.this-2');
 const task2 = document.createElement('div');
-  task2.className = 'task2';
+task2.className = 'task2';
 task2.innerHTML = `
 <div class="cross">
   <h2 class="h2-tag">${desktop.desktopSec.name}</h2>
@@ -384,23 +380,22 @@ task2.innerHTML = `
 `;
 body.append(task2);
 
-
 post2.addEventListener('click', () => {
   post2.classList.add('active');
   task2.classList.add('active');
   overlay.classList.add('active');
-})
+});
 const close2 = document.querySelector('.ex-2');
-close2.addEventListener('click',() => {
+close2.addEventListener('click', () => {
   close2.classList.remove('active');
   task2.classList.remove('active');
   overlay.classList.remove('active');
-})
+});
 
-//desktop third div
+// desktop third div
 const post3 = document.querySelector('.this-3');
 const task3 = document.createElement('div');
-  task3.className = 'task3';
+task3.className = 'task3';
 task3.innerHTML = `
 <div class="cross">
   <h2 class="h2-tag">${desktop.desktopTrd.name}</h2>
@@ -441,22 +436,22 @@ task3.innerHTML = `
 `;
 body.append(task3);
 
-
 post3.addEventListener('click', () => {
   post3.classList.add('active');
   task3.classList.add('active');
   overlay.classList.add('active');
-})
+});
 const close3 = document.querySelector('.ex-3');
-close3.addEventListener('click',() => {
+close3.addEventListener('click', () => {
   close3.classList.remove('active');
   task3.classList.remove('active');
   overlay.classList.remove('active');
-})
-//desktop fourth div
+});
+
+// desktop fourth div
 const post4 = document.querySelector('.this-4');
 const task4 = document.createElement('div');
-  task4.className = 'task4';
+task4.className = 'task4';
 task4.innerHTML = `
 <div class="cross">
   <h2 class="h2-tag">${desktop.desktopFth.name}</h2>
@@ -497,15 +492,14 @@ task4.innerHTML = `
 `;
 body.append(task4);
 
-
 post4.addEventListener('click', () => {
   post4.classList.add('active');
   task4.classList.add('active');
   overlay.classList.add('active');
-})
+});
 const close4 = document.querySelector('.ex-4');
-close4.addEventListener('click',() => {
+close4.addEventListener('click', () => {
   close4.classList.remove('active');
   task4.classList.remove('active');
   overlay.classList.remove('active');
-})
+});

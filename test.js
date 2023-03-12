8cf02ed2f462cbf56234bd0c129daf777a5eed72
Booze33 <tebetisloh@gmail.com>
2023-03-12 13:06:10 -0500

    fixing bugs

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-
 const email = document.getElementById('email');
 const error = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
-    if (email.value === email.value.toLowerCase()) {
-        form.submit();
-    } else {
-        event.preventDefault();
-        error.style.display = 'block';
-    }
-})
-
+  if (email.value === email.value.toLowerCase()) {
+    form.submit();
+  } else {
+    event.preventDefault();
+    error.style.display = 'block';
+  }
+});

a3731b0cac57faeba75f8162c7f2c062e723dd9b
Booze33 <tebetisloh@gmail.com>
2023-03-12 12:56:07 -0500

    finish validation

---
+++ test.js
@@ -0,0 +1,13 @@
+
+const email = document.getElementById('email');
+const error = document.getElementById('error');
+
+form.addEventListener('submit', (event) => {
+    if (email.value === email.value.toLowerCase()) {
+        form.submit();
+    } else {
+        event.preventDefault();
+        error.style.display = 'block';
+    }
+})
+
